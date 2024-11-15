03bceeea8a35dd97b191651d0cb300ba5ec1b44c
Avinash Vasipalli <avinash@Avinashs-MacBook-Pro.local>
2024-11-15 18:53:21 -0500

    New view file-upload page

--- 520_Project/csv-query-app/src/app/app.routes.ts
+++ 520_Project/csv-query-app/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { FileUploadComponent } from './pages/file-upload/file-upload.component';
 import { CsvRendererComponent } from './pages/csv-renderer/csv-renderer.component';
+import { MainPageComponent } from './pages/main-page/main-page.component'; // Corrected the import path
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
   { path: 'file-upload', component: FileUploadComponent },
   { path: 'csv-renderer', component: CsvRendererComponent },
+  { path: 'main-page', component: MainPageComponent }, // Make sure this line is correct
 ];
+
