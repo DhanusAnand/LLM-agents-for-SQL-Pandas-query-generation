d8bd15e7ed4608a45312a3551a5056263e62d713
md-hassan <mdhassan@umass.edu>
2024-11-19 18:57:57 -0500

    Added code for SQL Agent

--- 520_Project/backend/llm_agent.py
+++ 520_Project/backend/llm_agent.py
@@ -1,38 +1,29 @@
 import os
+import sqlite3
+import pandas as pd
 
-from langchain.agents.agent_types import AgentType
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+# Langchain imports
+from langchain_openai import OpenAI
+from sqlalchemy import create_engine
 from langchain_openai import ChatOpenAI
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+
+# Loading variables from .env file
 from dotenv import load_dotenv
-# loading variables from .env file
 load_dotenv()
 
-
-import pandas as pd
-from langchain_openai import OpenAI
-
-# df = pd.read_csv(
-#     "https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/titanic.csv"
-# )
-
-# agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True,  allow_dangerous_code= True,
-#                                      return_intermediate_steps=True)
-
-# res = agent.invoke("how many rows are there?")
-# print(res)
-
 PANDAS_AGENT_PROMPT = "give me the pandas code to get answer to the query: {query}"
+SQL_AGENT_PROMPT = "give me the SQL code to get answer to the query: {query}"
 
 def query_pandas_agent(df, query):
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True,  allow_dangerous_code= True,
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False,  allow_dangerous_code= True,
                                      return_intermediate_steps=True)
-
     prompt = PANDAS_AGENT_PROMPT.format(query=query)
     print(prompt)
     res = agent.invoke(prompt)
-    print(res)
     return res
-
 
 def process_pandas_result_to_json(res):
     data = {
@@ -47,6 +38,49 @@ def process_pandas_result_to_json(res):
         data["result"] = res['intermediate_steps'][0][-1]
     return data
 
-# query = "filter the data where age >=21"
-# res = query_pandas_agent(df, query)
-# print(process_pandas_result_to_json(res))
+def csv_to_sqlite(df, db_file, table_name):
+    """Converts a CSV file to a SQLite database table."""
+    conn = sqlite3.connect(db_file)
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    conn.close()
+
+def query_sql_agent(df, query):
+    db_file = "table_name.db"
+    table_name = "table_name"
+    csv_to_sqlite(df, db_file, table_name)
+    engine = create_engine(f"sqlite:///{db_file}")
+    db = SQLDatabase(engine)
+
+    agent_executor = create_sql_agent(ChatOpenAI(model="gpt-3.5-turbo", temperature=0), db=db, agent_type="openai-tools", 
+                                      verbose=False, agent_executor_kwargs = {"return_intermediate_steps": True})
+    prompt = SQL_AGENT_PROMPT.format(query=query)
+    print(prompt)
+    res = agent_executor.invoke(prompt)
+    
+    sql_queries = []
+    for (log, output) in res["intermediate_steps"]:
+        if log.tool == 'sql_db_query':
+            sql_queries.append(log.tool_input)
+    
+    os.remove(db_file)
+    return res, sql_queries
+
+def process_sql_result_to_json(res, query):
+    data = {
+        "query": query,
+    }
+    data['result'] = res['intermediate_steps'][-1][-1]
+    return data
+
+
+# Example Usage
+
+# df = pd.read_csv("https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/titanic.csv")
+# query = "filter rows where age > 21"
+# pandas_res = query_pandas_agent(df, query)
+# print(pandas_res['output'])
+# pandas_data = process_pandas_result_to_json(pandas_res)
+
+# sql_res, sql_query = query_sql_agent(df, query)
+# sql_data = process_sql_result_to_json(sql_res, sql_query)
+# print(sql_query)
